5791e009dcc76182234fb3e08e49c621d9a30124
SairamChinta <166792584+SairamChinta@users.noreply.github.com>
2025-10-16 11:09:40 +0530

    Time: 2 ms (11.92%) | Memory: 8.1 MB (84.12%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -4,13 +4,17 @@ public:
         if (s.length() != goal.length()) {
             return false;
         }
-        // Try all possible rotations of 's'
-        for (int i = 0; i < s.length(); i++) {
-            string rotated = s.substr(i) + s.substr(0, i);  
-            if (rotated == goal) {
-                return true;
-            }
-        }
-        return false;
+        string doubledS = s + s;
+        return doubledS.find(goal) != string::npos;
+        
+        //brute;
+        // // Try all possible rotations of 's'
+        // for (int i = 0; i < s.length(); i++) {
+        //     string rotated = s.substr(i) + s.substr(0, i);  
+        //     if (rotated == goal) {
+        //         return true;
+        //     }
+        // }
+        // return false;
     }
 };

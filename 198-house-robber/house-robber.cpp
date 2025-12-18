24c51c9266862752cdeae32ee6cbfb7544411931
SairamChinta <166792584+SairamChinta@users.noreply.github.com>
2025-12-18 08:50:49 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (89.06%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int prev = nums[0];
+        int prev2 = 0;
+        for(int i =1; i< n; i++){
+            int take = nums[i];
+            if(i > 1) take += prev2;
+            int notake = prev;
+            int curri = max(take, notake);
+            prev2 = prev;
+            prev = curri;
+        }return prev;
+    }
+};

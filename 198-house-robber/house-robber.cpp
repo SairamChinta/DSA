734d6059a9faea0d0dbf60dc62198148be050788
SairamChinta <166792584+SairamChinta@users.noreply.github.com>
2026-02-09 06:33:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (78.24%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -4,6 +4,7 @@ public:
         int n = nums.size();
         int prev = nums[0];
         int prev2 = 0;
+
         for(int i =1; i< n; i++){
             int take = nums[i];
             if(i > 1) take += prev2;
@@ -11,6 +12,7 @@ public:
             int curri = max(take, notake);
             prev2 = prev;
             prev = curri;
-        }return prev;
+        }
+        return prev;
     }
 };

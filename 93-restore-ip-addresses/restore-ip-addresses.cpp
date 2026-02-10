2543debd6b7fff3a04a38cfc334a17a00ad8b887
SairamChinta <166792584+SairamChinta@users.noreply.github.com>
2026-02-10 10:13:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (57.39%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> result;
+
+    void backtrack(string& s, int index, int parts, string current) {
+        if (parts == 4 && index == s.size()) {
+            result.push_back(current.substr(0, current.size() - 1));
+            return;
+        }
+
+        if (parts == 4 || index == s.size()) return;
+
+        for (int len = 1; len <= 3 && index + len <= s.size(); len++) {
+            string part = s.substr(index, len);
+
+            if ((part[0] == '0' && part.size() > 1) || stoi(part) > 255)
+                continue;
+
+            backtrack(s, index + len, parts + 1, current + part + ".");
+        }
+    }
+
+    vector<string> restoreIpAddresses(string s) {
+        backtrack(s, 0, 0, "");
+        return result;
+    }
+};

0447dc28de39ac497321beec458343b936f52d41
SairamChinta <166792584+SairamChinta@users.noreply.github.com>
2026-02-11 08:21:06 +0530

    Time: 14 ms (35.57%) | Memory: 13.9 MB (93.44%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        // 1. Sort the array to use the two-pointer technique
         sort(nums.begin(), nums.end());
         
         int n = nums.size();
-        int closestSum = nums[0] + nums[1] + nums[2]; // Initialize with the first possible triplet
+        int closestSum = nums[0] + nums[1] + nums[2]; //Initialize first possible triplet
 
         for (int i = 0; i < n - 2; ++i) {
             int left = i + 1;

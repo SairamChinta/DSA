8ab26571b352d5663363f5f731a17acb7214400b
SairamChinta <166792584+SairamChinta@users.noreply.github.com>
2026-02-15 11:26:46 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (31.23%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
        int max_profit = 0;
-
-        // Start from the second day (index 1)
         for (size_t i = 1; i < prices.size(); ++i) {
             // If today's price is higher than yesterday's, we capture the profit
             if (prices[i] > prices[i - 1]) {

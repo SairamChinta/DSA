b7251b89440537b9f420a6b65b18e8f76908c312
SairamChinta <166792584+SairamChinta@users.noreply.github.com>
2026-02-16 09:47:56 +0530

    Time: 44 ms (76.31%) | Memory: 28.9 MB (90.56%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -11,13 +11,11 @@ public:
             int right = n - 1;
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-
                 if (sum == 0) {
                     result.push_back({nums[i], nums[left], nums[right]});
                     while (left < right && nums[left] == nums[left + 1]) left++;
                     while (left < right && nums[right] == nums[right - 1]) right--;
-                    left++;
-                    right--;
+                    left++; right--;
                 } else if (sum < 0) {
                     left++;
                 } else {
